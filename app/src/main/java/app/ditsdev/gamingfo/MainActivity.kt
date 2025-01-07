1ce63c890d29c77f475d54108dc31ded286d4283
Gorthdzx <krislaksana123@gmail.com>
2025-01-07 19:19:07 +0700

    refactor:moved variable

--- app/src/main/java/app/ditsdev/gamingfo/MainActivity.kt
+++ app/src/main/java/app/ditsdev/gamingfo/MainActivity.kt
@@ -3,9 +3,10 @@ package app.ditsdev.gamingfo
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import app.ditsdev.gamingfo.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -14,10 +15,9 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val navView: BottomNavigationView = binding.bottomNavigation
+        navView.setupWithNavController(navController)
     }
 }
