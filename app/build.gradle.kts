7e070eca494744548f964d9cd00f3ca647425eef
Gorthdzx <krislaksana123@gmail.com>
2025-01-10 22:16:47 +0700

    chore:using imported all libs from core module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,15 +45,5 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(project(":core"))
 }
