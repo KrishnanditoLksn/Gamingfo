3f1c1acb57f12cd2e04fb088d7d7347c2bf62bb5
Gorthdzx <krislaksana123@gmail.com>
2025-01-12 19:56:24 +0700

    refactor:renamed data class to publisher entity

--- core/src/main/java/app/ditsdev/core/local/entity/PublisherEntity.kt
+++ core/src/main/java/app/ditsdev/core/local/entity/PublisherEntity.kt
@@ -5,7 +5,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class Publisher(
+data class PublisherEntity(
     @PrimaryKey
     @ColumnInfo(name = "idPublisher")
     val idPublisher: Int,
