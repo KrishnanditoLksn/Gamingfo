1fadf06b318dded0cbe50ba97ead1e55cf12840f
Gorthdzx <krislaksana123@gmail.com>
2025-01-13 18:18:49 +0700

    fix:set entity table name for game

--- core/src/main/java/app/ditsdev/core/local/entity/GameEntity.kt
+++ core/src/main/java/app/ditsdev/core/local/entity/GameEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "games")
 data class GameEntity(
     @PrimaryKey(autoGenerate = false)
     @ColumnInfo(name = "gameId")
