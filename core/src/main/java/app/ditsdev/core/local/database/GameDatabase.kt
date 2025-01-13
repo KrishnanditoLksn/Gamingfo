116973e3c05fd2022b38d10fe6d90788a9a05d96
Gorthdzx <krislaksana123@gmail.com>
2025-01-13 18:44:10 +0700

    feat:init game database class

---
+++ core/src/main/java/app/ditsdev/core/local/database/GameDatabase.kt
@@ -0,0 +1,13 @@
+package app.ditsdev.core.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import app.ditsdev.core.local.dao.GameDao
+import app.ditsdev.core.local.entity.GameEntity
+import app.ditsdev.core.local.entity.PublisherEntity
+
+
+@Database(entities = [GameEntity::class, PublisherEntity::class], version = 1, exportSchema = false)
+abstract class GameDatabase : RoomDatabase() {
+    abstract fun gameDao(): GameDao
+}
