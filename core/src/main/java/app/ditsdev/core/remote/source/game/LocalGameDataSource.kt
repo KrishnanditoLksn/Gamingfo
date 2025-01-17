4ed46c2200849a19efdbdbecc851db998ec95060
Gorthdzx <krislaksana123@gmail.com>
2025-01-17 21:55:26 +0700

    feat: facade pattern impl in local game data source

---
+++ core/src/main/java/app/ditsdev/core/remote/source/game/LocalGameDataSource.kt
@@ -0,0 +1,17 @@
+package app.ditsdev.core.remote.source.game
+
+import app.ditsdev.core.local.dao.GameDao
+import app.ditsdev.core.local.entity.GameEntity
+import io.reactivex.rxjava3.core.Flowable
+
+class LocalGameDataSource(private val gameDao: GameDao) {
+    fun getAllGames(): Flowable<List<GameEntity>> = gameDao.getAllGames()
+    fun getFavoriteGame(): Flowable<List<GameEntity>> = gameDao.getFavoriteGames()
+
+    fun setFavoriteGame(game: GameEntity, currentState: Boolean) {
+        game.isFavorite = currentState
+        gameDao.updateFavoriteGame(game)
+    }
+
+    fun insertGame(game: List<GameEntity>) = gameDao.insertGame(game)
+}
