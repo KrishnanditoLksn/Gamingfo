f9c73a1dc87b6fbf42310dd05c7820d5b45ba617
Gorthdzx <krislaksana123@gmail.com>
2025-01-19 14:13:15 +0700

    feat:consume get all games function in home viewmodel

--- app/src/main/java/app/ditsdev/gamingfo/ui/home/HomeFragment.kt
+++ app/src/main/java/app/ditsdev/gamingfo/ui/home/HomeFragment.kt
@@ -5,13 +5,48 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import app.ditsdev.gamingfo.R
+import androidx.recyclerview.widget.GridLayoutManager
+import app.ditsdev.core.result.resource.ResourceResult
+import app.ditsdev.gamingfo.databinding.FragmentHomeBinding
+import app.ditsdev.gamingfo.ui.GameListAdapter
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
+    private val homeViewModel: HomeViewModel by viewModel()
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.gameProgressBar.visibility = View.INVISIBLE
+        val gameAdapter = GameListAdapter()
+        homeViewModel.getAllGames().observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is ResourceResult.Success -> {
+                    binding.gameProgressBar.visibility = View.INVISIBLE
+                    gameAdapter.submitList(result.data)
+                }
+
+                is ResourceResult.Loading -> {
+                    binding.gameProgressBar.visibility = View.VISIBLE
+                }
+
+                is ResourceResult.Error -> {
+                    binding.gameProgressBar.visibility = View.INVISIBLE
+                }
+            }
+            binding.rvHeadlines.apply {
+                adapter = gameAdapter
+                setHasFixedSize(true)
+                layoutManager = GridLayoutManager(requireContext(), 2)
+            }
+        }
     }
 }
