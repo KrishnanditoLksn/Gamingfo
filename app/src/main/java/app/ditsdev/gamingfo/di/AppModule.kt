33decd31e0466606c37406f65a070c2fe9e805bb
Gorthdzx <krislaksana123@gmail.com>
2025-01-21 11:51:15 +0700

    feat:installed detail viewmodel module

--- app/src/main/java/app/ditsdev/gamingfo/di/AppModule.kt
+++ app/src/main/java/app/ditsdev/gamingfo/di/AppModule.kt
@@ -1,5 +1,6 @@
 package app.ditsdev.gamingfo.di
 
+import app.ditsdev.gamingfo.ui.detail.DetailGameViewModel
 import app.ditsdev.gamingfo.ui.home.HomeViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -7,5 +8,6 @@ import org.koin.dsl.module
 object AppModule {
     val viewModelModule = module {
         viewModel { HomeViewModel(get()) }
+        viewModel { DetailGameViewModel(get()) }
     }
 }
