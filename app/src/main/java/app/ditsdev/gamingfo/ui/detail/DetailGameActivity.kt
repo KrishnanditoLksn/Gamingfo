78e386ac6115d7429ecce00eba8792cec9b1a0f2
Gorthdzx <krislaksana123@gmail.com>
2025-01-21 11:52:23 +0700

    feat:updated favorite status using fab button and ensured its updated on isFavorite field in table

--- app/src/main/java/app/ditsdev/gamingfo/ui/detail/DetailGameActivity.kt
+++ app/src/main/java/app/ditsdev/gamingfo/ui/detail/DetailGameActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import app.ditsdev.core.domain.model.Game
+import app.ditsdev.gamingfo.R
 import app.ditsdev.gamingfo.databinding.ActivityDetailGameBinding
 import app.ditsdev.gamingfo.ui.GameListAdapter
 import coil3.load
@@ -23,7 +25,8 @@ class DetailGameActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val gameName = intent.getParcelableExtra(GameListAdapter.EXTRA_DATAS, Game::class.java)
-        binding.tvDetailGameName.text = gameName!!.gameName
+        var isFavorite = gameName!!.isFavorite
+        binding.tvDetailGameName.text = gameName.gameName
         binding.tvDetailRatingGame.rating = gameName.rating.toFloat()
         binding.ivDetailGame.load(gameName.backgroundImage)
         binding.tvDetailReleaseDate.text = gameName.released
@@ -31,9 +34,33 @@ class DetailGameActivity : AppCompatActivity() {
         binding.btnBack.setOnClickListener {
             backPressed()
         }
+        setFavoriteStatus(isFavorite)
+        binding.fabFavorite.setOnClickListener {
+            isFavorite = !isFavorite
+            detailVm.setFavoriteGames(gameName, isFavorite)
+            setFavoriteStatus(isFavorite)
+        }
     }
 
     private fun backPressed() {
         onBackPressedDispatcher.onBackPressed()
+    }
+
+    private fun setFavoriteStatus(isFavorite: Boolean) {
+        if (isFavorite) {
+            binding.fabFavorite.setImageDrawable(
+                ContextCompat.getDrawable(
+                    this,
+                    R.drawable.baseline_bookmark_24
+                )
+            )
+        } else {
+            binding.fabFavorite.setImageDrawable(
+                ContextCompat.getDrawable(
+                    this,
+                    R.drawable.baseline_bookmark_border_24
+                )
+            )
+        }
     }
 }
