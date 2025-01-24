e031bb0a6988da35e881dfec657538fdd9071ca8
Gorthdzx <krislaksana123@gmail.com>
2025-01-24 22:40:53 +0700

    feat:possible publisher dao query

---
+++ core/src/main/java/app/ditsdev/core/local/dao/PublisherDao.kt
@@ -0,0 +1,19 @@
+package app.ditsdev.core.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import app.ditsdev.core.local.entity.PublisherEntity
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Flowable
+
+@Dao
+interface PublisherDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertPublishers(publisher: List<PublisherEntity>): Completable
+
+    @Query("SELECT * FROM publishers")
+    fun displayAllPublishers(): Flowable<List<PublisherEntity>>
+}
