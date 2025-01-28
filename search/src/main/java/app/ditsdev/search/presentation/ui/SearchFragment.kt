fdb03ef2456579a5eeb8a6f991e589a02e9dbff7
Gorthdzx <krislaksana123@gmail.com>
2025-01-28 10:59:20 +0700

    feat:implement search feature using search view and search bar and displayed into list

--- search/src/main/java/app/ditsdev/search/presentation/ui/SearchFragment.kt
+++ search/src/main/java/app/ditsdev/search/presentation/ui/SearchFragment.kt
@@ -5,14 +5,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import app.ditsdev.search.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import app.ditsdev.search.databinding.FragmentSearchBinding
+import app.ditsdev.search.di.SearchModule
+import app.ditsdev.search.presentation.viewmodels.SearchViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.context.loadKoinModules
 
 class SearchFragment : Fragment() {
+    private val loadFeature by lazy { loadKoinModules(SearchModule.searchFeatureModule) }
+    private lateinit var binding: FragmentSearchBinding
+    private val searchViewModel: SearchViewModel by viewModel()
 
+    private fun injectSearchFeatures() = loadFeature
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    ): View {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val gameAdapter = GameResultAdapter()
+        with(binding) {
+            svGames.setupWithSearchBar(sbGames)
+            svGames
+                .editText.setOnEditorActionListener { _, _, _ ->
+                    sbGames.setText(svGames.text)
+                    svGames.hide()
+                    searchViewModel.searchGame(svGames.text.toString())
+                        .observe(viewLifecycleOwner) {
+                            gameAdapter.submitList(it)
+                        }
+                    false
+                }
+        }
+        binding.rvSearchResultGame.apply {
+            adapter = gameAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        injectSearchFeatures()
     }
 }
