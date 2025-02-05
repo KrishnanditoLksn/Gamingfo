91b8d87a03e11078a2c228e556be85adb30100a7
KrishnanditoLksn <krislaksana123@gmail.com>
2025-02-05 07:23:48 +0700

    feat:generate publisher dummy data

--- core/src/main/java/app/ditsdev/core/utils/DummyData.kt
+++ core/src/main/java/app/ditsdev/core/utils/DummyData.kt
@@ -1,6 +1,7 @@
 package app.ditsdev.core.utils
 
 import app.ditsdev.core.domain.model.Game
+import app.ditsdev.core.domain.model.Publisher
 
 object DummyData {
     fun generateDummyGame(): List<Game> {
@@ -12,6 +13,17 @@ object DummyData {
                 backgroundImage = "a.png",
                 released = "30-01-2024",
                 isFavorite = false
+            )
+        )
+    }
+
+    fun generateDummyPublisher(): List<Publisher> {
+        return listOf(
+            Publisher(
+                idPublisher = 308,
+                name = "Square Enix",
+                imageBackground = "https://media.rawg.io/media/games/54a/54a3e4c617217848dc43c4de9989fe37.jpg",
+                gamesCount = 600,
             )
         )
     }
