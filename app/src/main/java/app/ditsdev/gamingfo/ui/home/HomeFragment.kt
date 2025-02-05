b648a8b5c0a4ed69e7a9aad57685b872544a12c8
KrishnanditoLksn <krislaksana123@gmail.com>
2025-02-05 19:32:08 +0700

    refactor:set binding into lateinit var

--- app/src/main/java/app/ditsdev/gamingfo/ui/home/HomeFragment.kt
+++ app/src/main/java/app/ditsdev/gamingfo/ui/home/HomeFragment.kt
@@ -14,13 +14,12 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
     private val homeViewModel: HomeViewModel by viewModel()
-    private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
